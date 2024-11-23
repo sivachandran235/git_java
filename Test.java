3b73bc2ec1d96443e18b789fce0e3923cd7262c6
sivachandran235 <sivachandran235@gmail.com>
2024-11-23 10:40:32 +0530

    Update1 Test.java

--- Test.java
+++ Test.java
@@ -6,6 +6,12 @@ public static void main(String args[])
     System.out.println("elysium Academy");
     System.out.println("dfdsggfdghdfgfhfg");
      System.out.println("elysium academy");
+    System.out.println("********************************");
+
+    System.out.println("hi welcome to java");
+    System.out.println("elysium Academy");
+    System.out.println("dfdsggfdghdfgfhfg");
+     System.out.println("elysium academy");
     
   }
 }

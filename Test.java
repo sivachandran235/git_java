b94026f62333e42cb261136e9cefd434ea517b48
sivachandran235 <sivachandran235@gmail.com>
2025-01-05 13:29:35 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -19,7 +19,7 @@ public static void main(String args[])
     System.out.println("elysium Academy");
     System.out.println("dfdsggfdghdfgfhfg");
      System.out.println("elysium academy");
-    System.out.println("deepak");
-     System.out.println("Rahul");
+    System.out.println("sivachandran");
+     System.out.println("mani");
   }
 }
